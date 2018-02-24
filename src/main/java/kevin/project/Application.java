ffc83c078747fb4bc4d39426952d940c6ad1d49b
loukkevin <loukkevin@gmail.com>
2018-02-24 13:19:17 -0600

    add default request mapping

--- src/main/java/kevin/project/Application.java
+++ src/main/java/kevin/project/Application.java
@@ -6,14 +6,15 @@ import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.web.bind.annotation.ResponseBody;
 
 @SpringBootApplication
+@EnableAutoConfiguration
 public class Application {
 
 @RequestMapping("/")
-@ResponseBody
 String home() {
     return "Dars Parsing Service";
     }
