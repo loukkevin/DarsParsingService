155ad8b5dad6c4ed5508df0dd5611dc1cd946acd
loukkevin <loukkevin@gmail.com>
2018-02-24 13:42:59 -0600

    add default request mapping

--- src/main/java/kevin/project/DarsController.java
+++ src/main/java/kevin/project/DarsController.java
@@ -7,17 +7,15 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class DarsController {
 @Autowired ParseService parser;
 
-@CrossOrigin(origins = "https://obscure-shelf-53073.herokuapp.com/")
+@CrossOrigin(origins = "https://obscure-shelf-53073.herokuapp.com")
 @RequestMapping("/parse")
 	    public ProgramRequirements parseDarsFile(@RequestParam(value="darsURL", required=true) String url) throws IOException {
 	        return parser.parse(url);
 	    }
-            
 }
