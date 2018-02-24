9fc097b0c6d2c9aa532c429071d6b08e07bd2acd
loukkevin <loukkevin@gmail.com>
2018-02-24 13:03:34 -0600

    add default request mapping

--- src/main/java/kevin/project/DarsController.java
+++ src/main/java/kevin/project/DarsController.java
@@ -7,6 +7,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
@@ -18,4 +19,10 @@ public class DarsController {
 	    public ProgramRequirements parseDarsFile(@RequestParam(value="darsURL", required=true) String url) throws IOException {
 	        return parser.parse(url);
 	    }
+            
+@RequestMapping("/")
+@ResponseBody
+String home() {
+    return "Dars Parsing Service";
+    }
 }
