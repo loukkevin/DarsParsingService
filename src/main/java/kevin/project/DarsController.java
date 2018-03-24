9bac8e84ddb6bfcb057305a57853871916cc9e2d
loukkevin <loukkevin@gmail.com>
2018-03-24 15:51:28 -0500

    fix CORS bug

--- src/main/java/kevin/project/DarsController.java
+++ src/main/java/kevin/project/DarsController.java
@@ -14,14 +14,14 @@ public class DarsController {
 
 @Autowired ParseService parser;
 @Autowired DocumentHandler documentHandler;
-@CrossOrigin(origins = "https://scsu-gps.herokuapp.com/")
+@CrossOrigin(origins = "https://scsu-gps.herokuapp.com")
 @RequestMapping("/parse")
 	    public ProgramRequirements parseDarsFile(@RequestParam(value="darsURL", required=true) String url) throws IOException {
                 
 	        return parser.parse(documentHandler.createDocument(url));
 	    }
             
-@CrossOrigin(origins = "https://scsu-gps.herokuapp.com/")
+@CrossOrigin(origins = "https://scsu-gps.herokuapp.com")
 @RequestMapping("/getCourseInformation")
 	    public Course getCourseInformation(@RequestParam(value="name", required=true) String course) throws IOException {
 	        return parser.getCourseInformation(course);
